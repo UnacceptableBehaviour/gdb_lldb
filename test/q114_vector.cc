f14328b91ce96ec6b24d81092e8c271b9ed9c3a3
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-07-30 23:21:53 +0100

    thoughts

--- test/q114_vector.cc
+++ test/q114_vector.cc
@@ -87,6 +87,7 @@
 // std::cout << n << ", " << n << ", " << n << ", " << n << ", " << " \n";
 #include <iostream>
 #include <string>
+#include <algorithm>  // std::find, std::for_each
 
 // q114.cc
 #include <memory>
@@ -100,28 +101,57 @@ public:
 
 struct S {
   std::vector<C> v;
-  std::unique_ptr<C> u;
-  C *const p;
+  std::unique_ptr<C> u;   // object this points to deleted when this goes out of scope, is reset, or re-assigned
+  C *const p;             // constant pointer to type C
 
   S() 
-    : v(1) 
+    : v(1)         // std::vector<int> v (5);  // 5 default-constructed ints - v = { 0 0 0 0 0 };
     , u(new C())
-    , p(u.get())
+    , p(u.get())   // 
   {}
 };
 
-//int main() {
-//  S s;
-//  const S &r = s;
+int main() {
+  S s;                // ctor   v[0] = C,  u = &C,  p = &C
+  const S &r = s;     // r ref to s, r is const
+ 
+  s.v[0].foo();       // A
+  s.u->foo();         // A
+  s.p->foo();         // A    pointer constant not C
+
+  r.v[0].foo();       // B
+  r.u->foo();         // A
+  r.p->foo();         // A
+}
+
+
+// According to §[dcl.ptr]¶1 in the C++ Standard, "The cv-qualifiers [e.g., const] apply to the
+// pointer and not to the object pointed to."
 // 
-//  s.v[0].foo();
-//  s.u->foo();
-//  s.p->foo();
-//
-//  r.v[0].foo();
-//  r.u->foo();
-//  r.p->foo();
-//}
+// That is, const-ness is shallow with regards to raw pointers and references (and standard types
+// that seek to emulate them, like std::unique_ptr) but not with regard to standard containers such
+// as std::vector.
+// 
+// In the code above, the object s is non-const, and so its members all retain their default
+// const-ness and all calls through them invoke the non-const version of C::foo().
+// 
+// However, r refers to its object as a const instance of S.
+// That const-ness changes the behavior of its member v, an std::vector which is "const-correct"
+// in the sense that its operator§[] returns const C& (see [sequence.reqmts]¶14) and therefore
+// invokes the const version of C::foo().
+// 
+// The const-ness of r's referent is also propagated to its members u and p (meaning one could not
+// perform a mutating operation on u, e.g., calling r.u.reset()), but this has no effect on the instance
+// of C that they both point to. That is, the pointers themselves become const, but the pointed-to
+// objects remain non-const. Hence, they both still call the non-const version of C::foo().
+// 
+// The const-ness of the member S::p is the same for both s and r. Because it is declared as a const
+// pointer, it does not change const-ness to follow the const-ness of its instance of S but remains a
+// const pointer to a non-const object.
+
+
+
+
 
 
 // vector basics
@@ -133,58 +163,168 @@ struct S {
 // Implementation
 // https://timsong-cpp.github.io/cppwp/n4659/vector
 
-void pv(std::vector<int> v, std::string msg=""){
-    std::cout << msg << ": " << "v = { ";
-    for (int n : v) {
-        std::cout << n << " ";
-    }
-    std::cout << "}; \n";  
-}
-
-int main()
-{
-    // curly braces for initialisation
-    int ba = 5;
-    int bra{5};   // same as above
-    
-    std::string stra = "String initialised W/O braces";
-    std::string strab{"String initialised w/ braces"};
-    
-    std::vector<int> vv = { 7, 5, 16, 8 };
-    std::vector<int> vvb { 12, 2, 18, 4 };
-    
-    std::cout << ba << ", " << bra << ", " << stra << ", " << strab << ", "
-              << vv[0] << ", " << vvb[0] << ", " << " \n";
-
-
-
-    // std::cout << n << ", " << n << ", " << n << ", " << n << ", " << " \n";
-    
-    // Create a vector containing integers
-    std::vector<int> v = { 7, 5, 16, 8 };
- 
-    // Add two more integers to vector
-    v.push_back(25);
-    v.push_back(13);
- 
-    pv(v, "test print");
-    
-    // shift, unshift, push(push_back), pop(pop_back)
-    
-    
-    // find unique
-    
-    // find multiple
-    
-    
-    // splice
-    
-    
-    // iterating
-    
-    
-    // reverse iterating
-    
-    
-}
-
+//void pv(std::vector<int> v, std::string msg=""){
+//    std::cout << msg << ": " << "v = { ";
+//    for (int n : v) {
+//        std::cout << n << " ";
+//    }
+//    std::cout << "}; \n";  
+//}
+//
+//void pv(std::vector<int>::iterator iv, std::vector<int>::iterator end, std::string msg=""){
+//    std::cout << msg << ": " << "v = { ";
+//    //for (int n : v) { // error: invalid range expression of type 'std::__1::__wrap_iter<int *>'; no viable 'begin' function available
+//    for (; iv != end; iv++) {
+//        std::cout << *iv << " ";
+//    }
+//    std::cout << "}; \n";  
+//}
+//
+//void processItemFunc (int i){    // function
+//  std::cout << " " << i;
+//}
+//
+//struct classname {
+//  void operator() (int i) {
+//    std::cout << " " << i;
+//  }
+//} processorObj;
+//
+//int main()
+//{
+//    // curly braces for initialisation
+//    int ba = 5;
+//    int bra{5};   // same as prev line
+//    
+//    std::string stra = "String initialised W/O braces";
+//    std::string strab{"String initialised w/ braces"};    // same as prev line
+//    
+//    std::vector<int> vv = { 7, 5, 16, 8 };
+//    std::vector<int> vvb { 12, 2, 18, 4 };
+//    
+//    std::cout << ba << ", " << bra << ", " << stra << ", " << strab << ", "
+//              << vv[0] << ", " << vvb[0] << ", " << " \n";
+//
+//
+//
+//    // std::cout << n << ", " << n << ", " << n << ", " << n << ", " << " \n";
+//    std::vector<int> myvec (5);  // 5 default-constructed ints
+//    pv(myvec, "myvec (5)");      // v = { 0 0 0 0 0 };
+//    
+//    // Create a vector containing integers
+//    std::vector<int> v = { 7, 5, 16, 8, 77, -5 };
+//    pv(v, "contents");
+//    
+//    // shift, unshift, push(push_back), pop(pop_back)
+//    v.push_back(25);
+//    pv(v, "push_back");
+//    v.push_back(13);    
+//    pv(v, "push_back");
+//
+//    v.pop_back();
+//    pv(v, "pop_back");
+//    v.pop_back();    
+//    pv(v, "pop_back");
+//    
+//    // BETTER OFF USING DEQUE! if need shift/unshift
+//    // 1st arg is an (random access iterator) iterator not an index
+//    // shift
+//    v.insert(v.begin(), 88);  // ** Linear complexity on the number of elements after position (moving).
+//    pv(v, "shift - insert");
+//    
+//    // shift
+//    // emplace can construct a vector element based on args
+//    v.emplace(v.begin(), 99);  // ** Linear complexity on the number of elements after position (moving).
+//    pv(v, "shift - emplace");
+//    
+//    // unshift
+//    
+//    
+//    // find unique - #include <algorithm> std::find(iter.begin, iter.end, target) return iter to target or iter.end
+//    // unless vector is sorted find checks all locations - slow
+//    // https://stackoverflow.com/questions/571394/how-to-find-out-if-an-item-is-present-in-a-stdvector
+//    // suggest map or hash_set
+//    // find 7 in vector
+//    int target = 7;
+//    if ( std::find(v.begin(), v.end() , target) != v.end() )
+//      std::cout << "Found " << target << '\n';
+//    else
+//      std::cout << target << " NOT Found " << '\n';
+//
+//    std::vector<int>::iterator itag = std::find(v.begin(), v.end() , target);      
+//    if ( itag != v.end() )
+//      // int index = itag - v.begin(); // this arithmatic doesn't appear to return int - returns iterator
+//      // std::cout << "Found " << target << " at index " << index << " content="<< *itag << " or " << v[index] <<'\n';      
+//      std::cout << "Found " << target << " at index " << itag - v.begin() << " content="<< *itag << " or " << v[itag - v.begin()] <<'\n';
+//    else
+//      std::cout << target << " NOT Found " << '\n';
+//    
+//    
+//    // find multiple - see for_each
+//    
+//    
+//    // splice
+//    pv(v, "slice vector untouched: ");
+//    std::vector<int> v2 = std::vector<int>(v.begin() + 1, v.begin() + 5); // doesn't include last pos!
+//    pv(v2, "slice vector 1-4: ");
+//    
+//    // - - - scanning
+//    // using index
+//    std::cout << "GOF for loop - v contains: ";
+//    for (int i=0; i < v.size(); i++)
+//      std::cout << v[i] << ' ';
+//    std::cout << '\n';
+//    
+//    // iterating
+//    std::vector vtr = {'A', 'B', 'C', 'D', 'E'};
+//    //for (std::vector::iterator it=vtr.begin(); it != vtr.end(); it++) // error: 'vector' is not a class, namespace, or enumeration
+//    //for (std::vector<int>::iterator it=vtr.begin(); it != vtr.end(); it++) // error: no viable conversion from '__wrap_iter<std::__1::vector<char, std::__1::allocator<char> >::pointer>' to '__wrap_iter<std::__1::vector<int, std::__1::allocator<int> >::pointer>'
+//    for (std::vector<char>::iterator it=vtr.begin(); it != vtr.end(); it++)  // OK - char not int
+//    //for (auto it=vtr.begin(); it != vtr.end(); it++) // OK
+//        std::cout << *it << ' ';
+//    std::cout << std::endl;    
+//
+//    // neater
+//    std::cout << "v contains:";
+//    for (auto& x: vtr)    // range notation
+//      std::cout << ' ' << x;
+//    std::cout << '\n';    
+//    
+//    
+//    // for_each(v.begin(), v.end(), func/method)    #include <algorithm> std::for_each
+//    std::cout << "for_each function call:";
+//    std::for_each(v.begin(), v.end(), processItemFunc);
+//    std::cout << '\n';    
+//
+//    std::cout << "for_each function object call:";
+//    std::for_each(v.begin(), v.end(), processorObj);
+//    std::cout << '\n';    
+//
+//    
+//    // REVERSE iterating
+//    std::cout << "reverse:";
+//    std::vector<int>::reverse_iterator rit;// = v.rbegin();
+//    for (rit = v.rbegin(); rit!= v.rend(); ++rit)  // v.end()
+//      std::cout << ' ' << *rit;
+//    std::cout << '\n';
+//    
+//    //if (v.rbegin() == v.end()) std::cout << "v.rbegin() == v.end()" << std::endl;
+//    
+//    //std::vector<int>::iterator rit = v.rbegin();
+//    //pv(rit);
+//    pv(v, "pv forward");
+//    pv(v.begin(), v.end(), "iterator f()");
+//      
+//    //std::cout << "reverse:";
+//    //std::vector<int>::reverse_iterator rit2 = v.rbegin();
+//    //for (rit2 = v.end(); rit2!= v.begin()-1; --rit)
+//    //  std::cout << ' ' << *rit;
+//
+//    
+//    //for (std::vector<char>::iterator it=x.rend(): vtr) // nope
+//    ////for (std::vector<char>::iterator x.rend(); it != vtr.end(); it++)
+//    //  std::cout << ' ' << x;
+//    //std::cout << '\n';      
+//    
+//}
+//
